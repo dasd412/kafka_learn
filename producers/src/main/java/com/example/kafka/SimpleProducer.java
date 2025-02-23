b404730559f96928da832d75b8d3dae849586461
dasd <dasd412@naver.com>
2025-02-23 10:59:47 +0900

    simple producer

--- producers/src/main/java/com/example/kafka/SimpleProducer.java
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -1,7 +1,33 @@
 package com.example.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 public class SimpleProducer {
+    public static void main(String[] args) {
+        String topicName="simple-topic";
 
+        // kafkaProducer configuration setting
+        Properties props=new Properties();
+        //bootstrap.servers, key.serializer.class, value.serializer.class
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.56.101:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        // kafkaProducer object creation
+        KafkaProducer<String,String>kafkaProducer= new KafkaProducer<>(props);
+
+        //ProducerRecord should match key,value serializer class
+        ProducerRecord<String,String>producerRecord=new ProducerRecord<>(topicName,"id-001","hello world");
+
+        //kafkaProducer message send
+        kafkaProducer.send(producerRecord);
+
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
 }
